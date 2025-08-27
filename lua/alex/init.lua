7a81b7a34ceb535a1ab41b1d2f62a8a95d15c084
Ademiju Adewole <adewolealex1@gmail.com>
2025-08-27 19:46:32 +0100

    initial

---
+++ lua/alex/init.lua
@@ -0,0 +1,27 @@
+require('alex.lazy')
+require('alex.lsp')
+require('alex.floaterm').setup()
+vim.cmd.colorscheme('github_dark_default')
+vim.cmd.set('splitright')
+vim.o.completeopt = "menuone,noinsert"
+vim.o.indentexpr = "nvim_treesitter#indent()"
+
+-- local blink = require('blink.cmp')
+-- local function split_signature()
+-- 	if not blink.show_documentation() then
+-- 		vim.print("couldn't show signature")
+-- 		return
+-- 	end
+-- 	local text = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+-- 	if text == "" then
+-- 		return
+-- 	end
+-- 	local bufid = vim.api.nvim_create_buf(false, true)
+-- 	if bufid == 0 then
+-- 		return
+-- 	end
+-- 	vim.api.nvim_buf_set_lines(bufid, 0, -1, false, text)
+-- 	vim.cmd('vsplit')
+-- 	vim.api.nvim_set_current_buf(bufid)
+-- end
+-- vim.keymap.set('n', '<leader>d', split_signature)
---
+++ lua/alex/lsp.lua
@@ -0,0 +1,32 @@
+vim.diagnostic.config{ virtual_text = true }
+
+local filetype_tabsize_table = {
+	html = 2,
+	css = 2,
+}
+
+vim.api.nvim_create_autocmd('LspAttach', {
+	callback = function (_)
+		local tab_width = filetype_tabsize_table[vim.bo.filetype]
+		if tab_width ~= nil then
+			vim.bo.tabstop = tab_width
+			vim.bo.shiftwidth = tab_width
+		end
+	end
+})
+
+vim.api.nvim_create_user_command('LspRestart', function ()
+	local lsps = vim.lsp.get_clients()
+	for _, lsp in ipairs(lsps) do
+		local name = lsp.name
+		vim.lsp.enable(name, false)
+		vim.lsp.enable(name, true)
+	end
+end, { desc = "restarts all running language servers" })
+
+vim.lsp.enable('luals')
+vim.lsp.enable('ts_ls')
+vim.lsp.enable('vscode_css_lsp')
+vim.lsp.enable('vscode_html_lsp')
+vim.lsp.enable('clangd')
+vim.lsp.enable('emmet_language_server')
